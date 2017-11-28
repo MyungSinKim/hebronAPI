f62e6bef45947b5955d357ff084f7b9907ac12b6
Rocky Lee <rocky.lee@payfirma.com>
2017-11-28 15:37:08 -0800

    update

---
+++ src/main/java/ca/vanhebron/restapi/models/Greeting.java
@@ -0,0 +1,24 @@
+package ca.vanhebron.restapi.models;
+
+/**
+ * Created by rocky.lee on 2017-11-28.
+ */
+public class Greeting {
+
+	private final long id;
+	private final String content;
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
